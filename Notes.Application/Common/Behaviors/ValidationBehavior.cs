dc7ffe758586fe250855b780be4bc0d1dfbd917d
HeadHocok <german.aleksandrovsky@yandex.ru>
2023-06-30 19:15:42 +0300

    валидация данных с Fluent API

---
+++ Notes.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using FluentValidation;
+
+namespace Notes.Application.Common.Behaviors
+{
+    /// <summary>
+    /// Встраивает реализацию пайплайн-поведения для валидации запросов в приложении. 
+    /// </summary>
+    public class ValidationBehavior<TRequest, TResponse> 
+        : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse> 
+        //where TRequest реализует TResponse, гарантируя необходимую структуру для дальнейшей работы.
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators; //принимает коллекцию валидаторов.
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators) =>
+            _validators = validators;
+
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+            CancellationToken cancellationToken)
+        {
+            var context = new ValidationContext<TRequest>(request);
+            var failures = _validators
+                .Select(v => v.Validate(context)) //вызывается для каждого валидатора. Выполняется параллельно.
+                .SelectMany(result => result.Errors)
+                .Where(failure => failure != null)
+                .ToList();
+            if (failures.Count != 0) //если возникли ошибки в ходе метода - выбрасываем исключения.
+            {
+                throw new ValidationException(failures);
+            }
+            return next();
+        }
+    }
+}
--- Notes.Application/DependencyInjection.cs
+++ Notes.Application/DependencyInjection.cs
@@ -1,18 +1,24 @@
 ﻿using System.Reflection;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+using Notes.Application.Common.Behaviors;
 
 namespace Notes.Application
 {
     /// <summary>
     /// Регистрирует контекст базы данных для медиатора.
+    /// Добавляет все необходимые зависимости для работы с MediatR и валидацией команд в приложении.
     /// </summary>
     public static class DependencyInjection
     {
         public static IServiceCollection AddApplication(
             this IServiceCollection services)
         {
-            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly())); //Регистрирует все сервисы 
+            services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() }); //Добавляет все валидаторы из текущей сборки
+            services.AddTransient(typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>)); //представляет поведение, которое выполняет валидацию команд перед их обработкой.
             return services;
         }
     }
