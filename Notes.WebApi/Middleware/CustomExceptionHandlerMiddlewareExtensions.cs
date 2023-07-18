e172ba266b25d8152b378937a1e522f09330c08a
HeadHocok <german.aleksandrovsky@yandex.ru>
2023-07-19 02:24:12 +0300

    adding authentication rules

---
+++ Notes.WebApi/Middleware/CustomExceptionHandlerMiddlewareExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Notes.WebApi.Middleware
+{
+    /// <summary>
+    /// Добавляем обработчик ошибок (Middleware) в пайплайн.
+    /// </summary>
+    public static class CustomExceptionHandlerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomExceptionHandler(this
+            IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomExceptionHandlerMiddleware>();
+        }
+    }
+}
--- Notes.WebApi/Program.cs
+++ Notes.WebApi/Program.cs
@@ -1,53 +1,98 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Reflection;
 using Notes.Application;
 using Notes.Application.Common.Mappings;
 using Notes.Application.Interfaces;
 using Notes.Persistence;
+using Notes.WebApi.Middleware;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddApplication();
-builder.Services.AddControllers();
-builder.Services.AddPersistence(builder.Configuration);
-builder.Services.AddAutoMapper(config =>
+namespace Notes.WebApi
 {
-    config.AddProfile(new AssemblyMappingProfile(typeof(Program).Assembly));
-    config.AddProfile(new AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
-});
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy =>
+    public class Program
     {
-        policy.AllowAnyHeader();
-        policy.AllowAnyMethod();
-        policy.AllowAnyOrigin();
-    });
-});
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
 
-var app = builder.Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                try
+                {
+                    var context = serviceProvider.GetRequiredService<NotesDbContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception exception)
+                {
+                    // Обработка исключения при инициализации базы данных
+                    Console.WriteLine(exception);
+                }
+            }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
+            host.Run();
+        }
 
-using (var scope = app.Services.CreateScope())
-{
-    try
-    {
-        var context = scope.ServiceProvider.GetRequiredService<NotesDbContext>();
-        DbInitializer.Initialize(context);
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureServices((hostContext, services) =>
+                    {
+                        var configuration = hostContext.Configuration;
+
+                        services.AddAutoMapper(config =>
+                        {
+                            config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+                            config.AddProfile(new AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
+                        });
+
+                        services.AddApplication();
+                        services.AddPersistence(configuration);
+                        services.AddControllers();
+
+                        services.AddCors(options =>
+                        {
+                            options.AddPolicy("AllowAll", policy =>
+                            {
+                                policy.AllowAnyHeader();
+                                policy.AllowAnyMethod();
+                                policy.AllowAnyOrigin();
+                            });
+                        });
+
+                        services.AddAuthentication(config => //Прохождение аутентификации в приложении
+                        {
+                            config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                            config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                        })
+                        .AddJwtBearer("Bearer", options =>
+                        {
+                            options.Authority = "https://localhost:7049/"; //Наш URL сервера
+                            options.Audience = "NotesWebAPI";
+                            options.RequireHttpsMetadata = false;
+                        });
+                    });
+
+                    webBuilder.Configure(app =>
+                    {
+                        var env = app.ApplicationServices.GetRequiredService<IWebHostEnvironment>();
+                        if (env.IsDevelopment())
+                        {
+                            app.UseDeveloperExceptionPage();
+                        }
+
+                        app.UseCustomExceptionHandler();
+                        app.UseRouting();
+                        app.UseHttpsRedirection();
+                        app.UseCors("AllowAll");
+                        app.UseAuthentication();
+                        app.UseAuthorization();
+
+                        app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapControllers();
+                        });
+                    });
+                });
     }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex);
-    }
 }
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.UseCors("AllowAll");
-app.MapControllers();
-
-app.Run();
